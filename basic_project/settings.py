2bcbaf2751147935a5ce33120c6a2fd0cf6e62b7
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-01-13 10:24:27 +0900

    chaging password working

--- basic_project/settings.py
+++ basic_project/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+APPEND_SLASH = False
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Replace with your React app's URL
