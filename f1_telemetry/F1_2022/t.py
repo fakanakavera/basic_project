6a9dbe032980ecdc30885d6124ce0647ea715d72
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-04-05 20:52:25 +0900

    a

--- f1_telemetry/F1_2022/t.py
+++ f1_telemetry/F1_2022/t.py
@@ -2,3 +2,6 @@ from f1_22_telemetry.listener import TelemetryListener
 
 listener = TelemetryListener(port=20777, host='localhost')
 packet = listener.get()
+
+# TRUNCATE TABLE f1_telemetry_carmotion, f1_telemetry_carsetup, f1_telemetry_cartelemetry, f1_telemetry_header, f1_telemetry_lap, f1_telemetry_log, f1_te
+# lemetry_packetsession, f1_telemetry_participant RESTART IDENTITY;
