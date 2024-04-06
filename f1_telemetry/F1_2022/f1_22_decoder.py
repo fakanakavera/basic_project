890f982b82d5250ca9e59a6a074a6de11b449883
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-04-06 13:06:04 +0900

    a

--- f1_telemetry/F1_2022/f1_22_decoder.py
+++ f1_telemetry/F1_2022/f1_22_decoder.py
@@ -157,9 +157,9 @@ class f1_22_decoder:
         self.driver_id = []
         for i in range(self.total_participants):
             self.driver_id.append(0)
-        for p in range(0, self.total_participants):
+        for p in range(0, self.total_participants+1):
             print('-----------------------------------------\nParticipant: ', p+1, ' of ', ParticipantsData[0][1])
-            for x in range(1, len(ParticipantsData)):
+            for x in range(0, len(ParticipantsData)):
                 
                 self.size = data_types[ParticipantsData[x][2]]['size']
                 ParticipantsData[x][1] = unpack(
