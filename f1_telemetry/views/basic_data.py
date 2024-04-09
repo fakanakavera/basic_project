90e01cca6ec1f1dc630d5d31de5faaf4826eedf3
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-04-09 22:42:03 +0900

    da

--- f1_telemetry/views/basic_data.py
+++ f1_telemetry/views/basic_data.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..models import Header
 
-def unique_sessionUIDs():
+def unique_sessionUIDs(request):
     """
     Retrieve unique sessionUIDs from the Headers data.
     """
