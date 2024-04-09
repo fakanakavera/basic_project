2ba457bdf706e07e328679c9a5bc5ec15733fbd8
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-04-09 22:50:50 +0900

    asd

--- basic_project/settings.py
+++ basic_project/settings.py
@@ -62,6 +62,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
 }
 
 ROOT_URLCONF = 'basic_project.urls'
