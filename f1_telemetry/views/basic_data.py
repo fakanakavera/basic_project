8e2f9d911688e3d37e3a833a78f4f14e360e1565
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-04-09 22:34:16 +0900

    unique UIDs

---
+++ f1_telemetry/views/basic_data.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from f1_telemetry.models import Header
+
+def unique_sessionUIDs(request):
+    """
+    Retrieve unique sessionUIDs from the Headers data.
+    """
+    # Fetch unique sessionUIDs from the CarTelemetry data
+    unique_sessionUIDs = Header.objects.values_list('sessionUID', flat=True).distinct()
+    print(f"unique_sessionUIDs: {unique_sessionUIDs}\n")    
+    return Response(unique_sessionUIDs)
