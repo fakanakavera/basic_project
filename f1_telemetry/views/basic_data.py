41301255bc0629a0fcaf4e03f6d35d8c7e715035
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-04-13 19:58:23 +0900

    a

--- f1_telemetry/views/basic_data.py
+++ f1_telemetry/views/basic_data.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from funcs import unique_sessionUIDs, get_trackid_from_sessionUID, get_all_headers_from_sessionUID, get_all_telemetry_from_sessionUID
+from .funcs import unique_sessionUIDs, get_trackid_from_sessionUID, get_all_headers_from_sessionUID, get_all_telemetry_from_sessionUID
 
 @api_view(['GET'])
 def unique_sessionUIDs(request):
