c1512b0c75d9f8f2b158fa704e95a5744070789e
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-04-13 18:38:22 +0900

    trackids

--- f1_telemetry/views/basic_data.py
+++ f1_telemetry/views/basic_data.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from ..models import Header
+from ..models import Header, PacketSession
 
 @api_view(['GET'])
 def unique_sessionUIDs(request):
@@ -10,4 +10,9 @@ def unique_sessionUIDs(request):
     # Fetch unique sessionUIDs from the CarTelemetry data
     unique_sessionUIDs = Header.objects.values_list('sessionUID', flat=True).distinct()
     unique_sessionUIDs = list(unique_sessionUIDs)
+    sessionUID_to_trackid_map = {}
+    for sessionUID in unique_sessionUIDs:
+        trackids = PacketSession.objects.filter(header__sessionUID=sessionUID).values_list('trackid', flat=True)
+        sessionUID_to_trackid_map[sessionUID] = list(trackids)
+    print(sessionUID_to_trackid_map)
     return Response(unique_sessionUIDs)
