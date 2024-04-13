afb358ed0aab5df4a6849e037b8821d82590eda7
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-04-13 19:25:27 +0900

    as

--- f1_telemetry/views/basic_data.py
+++ f1_telemetry/views/basic_data.py
@@ -7,7 +7,6 @@ def unique_sessionUIDs(request):
     """
     Retrieve unique sessionUIDs from the Headers data.
     """
-    print('unique_sessionUIDs')
     # Fetch unique sessionUIDs from the CarTelemetry data
     unique_sessionUIDs = Header.objects.values_list('sessionUID', flat=True).distinct()
     unique_sessionUIDs = list(unique_sessionUIDs)
@@ -15,9 +14,8 @@ def unique_sessionUIDs(request):
     for sessionUID in unique_sessionUIDs:
         trackids = PacketSession.objects.filter(header__sessionUID=sessionUID).values_list('trackId', flat=True).distinct()
         sessionUID_to_trackid_map[sessionUID] = list(trackids)
-    print(sessionUID_to_trackid_map)
-    return Response([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    # return Response(sessionUID_to_trackid_map)
+
+    return Response(sessionUID_to_trackid_map)
 
 @api_view(['GET'])
 def test_api(request):
