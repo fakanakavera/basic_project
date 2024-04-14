4e25c2274b9a574f97aa83d3df5e177c3dbb90c3
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-04-14 10:15:00 +0900

    asd

--- f1_telemetry/urls.py
+++ f1_telemetry/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views.basic_data import get_all_headers_from_sessionUID, get_trackid_from_sessionUID, unique_sessionUIDs, get_all_telemetry_from_sessionUID
+from .views.basic_data import test_api, get_all_headers_from_sessionUID, get_trackid_from_sessionUID, unique_sessionUIDs, get_all_telemetry_from_sessionUID
 
 urlpatterns = [
-    path('test-api/<int:sessionUID>/<int:lap_num>/', get_all_telemetry_from_sessionUID, name='test-api'),
+    path('test-api/<int:sessionUID>/<int:lap_num>/', test_api, name='test-api'),
     path('get-all-headers-from-sessionUID/<int:sessionUID>/', get_all_headers_from_sessionUID, name='get-all-headers-from-sessionUID'),
     path('get-track-from-sessionUID/<int:sessionUID>/', get_trackid_from_sessionUID, name='get-track-from-sessionUID'),
     path('unique-sessionUIDs/', unique_sessionUIDs, name='unique-sessionUIDs'),
