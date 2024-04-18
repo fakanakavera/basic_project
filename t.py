6127ae7d0c1ef3b750b0f9528aa4cc30df4d19ae
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-04-18 20:46:30 +0900

    v1.5

--- t.py
+++ t.py
@@ -1,6 +1,6 @@
 from f1_telemetry.F1_2022.f1_22_decoder import f1_22_decoder
 from f1_telemetry.F1_2022.f1_22_decoder_v2 import f1_22_decoder_v2
 
-d = f1_22_decoder_v2()
+d = f1_22_decoder()
 
 d.decoder_loop()
